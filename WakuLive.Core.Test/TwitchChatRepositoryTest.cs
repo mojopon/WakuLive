493e4a08fd9490bf12e9e35a34b6f0af8235ea95
Anomaloris <tyamuro@gmail.com>
2023-03-02 17:54:54 +0900

    Core用のテストプロジェクトを追加

---
+++ WakuLive.Core.Test/TwitchChatRepositoryTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WakuLive.Core.Test
+{
+    public class TwitchChatRepositoryTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ WakuLive.Core.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace WakuLive.Core.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
