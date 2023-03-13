7ee4aa3d265dc8d4a829107f700a55fb42cad52d
Anomaloris <tyamuro@gmail.com>
2023-03-13 15:46:02 +0900

    LiveStreamChatListBoxのテンプレート作成機能の実装

---
+++ WakuLive/View/Channel/DataTemplateSelector/ListBoxItemDataTemplateSelector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using WakuLive.View.Channel.DataTemplateSelector;
+using WakuLive.VM;
+
+namespace WakuLive.View
+{
+    public class ListBoxItemDataTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var vm = (LiveStreamChatListBoxItemViewModel)item;
+            var dataTemplate = LiveStreamChatListBoxItemDataTemplates.GetDataTemplateForUserComment(vm);
+            return dataTemplate;
+        }
+    }
+}
---
+++ WakuLive/View/Channel/DataTemplateSelector/LiveStreamChatListBoxItemDataTemplates.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using WakuLive.VM;
+
+namespace WakuLive.View.Channel.DataTemplateSelector
+{
+    public static class LiveStreamChatListBoxItemDataTemplates
+    {
+        public static DataTemplate GetDataTemplateForUserComment(LiveStreamChatListBoxItemViewModel vm) 
+        {
+            var dataTemplate = new DataTemplate();
+
+            var wrapPanelFactory = new FrameworkElementFactory(typeof(WrapPanel));
+
+            var nameTextBlock = new FrameworkElementFactory(typeof(TextBlock));
+            nameTextBlock.SetValue(TextBlock.TextProperty, vm.Name);
+
+            var separatorTextBlock = new FrameworkElementFactory(typeof(TextBlock));
+            separatorTextBlock.SetValue(TextBlock.TextProperty, ": ");
+
+            var commentTextBlock = new FrameworkElementFactory(typeof(TextBlock));
+            commentTextBlock.SetValue(TextBlock.TextProperty, vm.Comment);
+
+            wrapPanelFactory.AppendChild(nameTextBlock);
+            wrapPanelFactory.AppendChild(separatorTextBlock);
+            wrapPanelFactory.AppendChild(commentTextBlock);
+
+            dataTemplate.VisualTree = wrapPanelFactory;
+            return dataTemplate;
+        }
+    }
+}
